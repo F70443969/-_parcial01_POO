3be71f21f7c0c5eba1e9bc5d22948e1084375a24
F70443969 <54407072+F70443969@users.noreply.github.com>
2020-04-19 21:20:21 -0600

    Creating do, if and  swicht

--- PARCIAL O1/src/com/FARC/x00022319/Main.java
+++ PARCIAL O1/src/com/FARC/x00022319/Main.java
@@ -16,3 +16,51 @@ public class Main {
         Empresa Objeto =  new Empresa(nombreEmpresa);
 
         nombreE();
+
+        do {
+            pedir();
+            switch (opc) {
+                case 1:
+
+                    opcServicioOPlazaM();
+
+                    switch (opcServicioOPlaza) {
+                        case 1:
+                            Objeto.addEmpleado(addServicioProfesionales());
+                            break;
+                        case 2:
+                            Objeto.addEmpleado(addPlazafija());
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, dariaError());
+                            break;
+                    }
+                    ;
+                    break;
+
+
+                case 2:
+                    if (Objeto.getPlanilla().isEmpty()) {
+
+                        JOptionPane.showMessageDialog(null, "La lista esta vacia, no hay empleados aun ");
+                    } else {
+                        nombre = JOptionPane.showInputDialog(null, "Digite el nombre de la persona que desea despedir: ");
+                        try {
+                            Objeto.quitEmpleado(nombre);
+                        } catch (ConcurrentModificationException e) {
+                            JOptionPane.showMessageDialog(null, "La lista esta vacia.");
+                        }
+                    }
+                    break;
+
+                case 3:
+
+                    if (Objeto.getPlanilla().isEmpty()) {
+
+                        JOptionPane.showMessageDialog(null, "La lista esta vacia ");
+                    } else {
+                        JOptionPane.showMessageDialog(null, Objeto.getPlanilla() + "\n");
+                    }
+                    break;
