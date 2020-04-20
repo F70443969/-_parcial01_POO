08eb5dac2b482389afe3bea95cee9ede29da2c2c
F70443969 <54407072+F70443969@users.noreply.github.com>
2020-04-19 18:46:04 -0600

    Uploading more clases

--- PARCIAL O1/src/com/FARC/x00022319/PlazaFija.java
+++ PARCIAL O1/src/com/FARC/x00022319/PlazaFija.java
@@ -1,4 +1,11 @@
 package com.FARC.x00022319;
 
-public class PlazaFija {
+public class PlazaFija extends Empleado {
+    private int extension = 0;
+
+    public PlazaFija(String nombre, String puesto, double salario, int extension) {
+        super(nombre, puesto, salario);
+        this.extension = extension;
+    }
+
 }
--- PARCIAL O1/src/com/FARC/x00022319/ServicioProfesional.java
+++ PARCIAL O1/src/com/FARC/x00022319/ServicioProfesional.java
@@ -1,4 +1,10 @@
 package com.FARC.x00022319;
 
-public class ServicioProfesional {
+public class ServicioProfesional extends Empleado {
+    private int MesesContratado = 0;
+
+    public ServicioProfesional(String nombre, String puesto, double salario, int mesesContratado) {
+        super(nombre, puesto, salario);
+        MesesContratado = mesesContratado;
+    }
 }
