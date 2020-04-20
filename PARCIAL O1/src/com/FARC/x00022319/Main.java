2cbf53049d9afe231a7130e249005d1e0fa6a952
F70443969 <54407072+F70443969@users.noreply.github.com>
2020-04-19 21:18:02 -0600

    Starting Class Main with atributes and variablesnd

--- PARCIAL O1/src/com/FARC/x00022319/Main.java
+++ PARCIAL O1/src/com/FARC/x00022319/Main.java
@@ -1,8 +1,18 @@
 package com.FARC.x00022319;
 
+import javax.swing.*;
+import java.util.ConcurrentModificationException;
+
 public class Main {
 
+    static byte opc2 = 0, opc = 0, opcServicioOPlaza = 0;
+    static String nombre, puesto, nombreEmpresa;
+    static double salario = 0;
+    static int mesesDeContrato = 0, extension = 0;
+
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        String nombre, puesto, nombreEmpleado;
+
+        Empresa Objeto =  new Empresa(nombreEmpresa);
+
+        nombreE();
